2fec272ec96f85001348d34ea56c290b8a3d1d74
vinicius pacheco <vinipachecov@gmail.com>
2017-07-31 16:29:00 -0300

    Class Based Views

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # djangoecommerce
-Projeto de didático utilizado para meu curso de Django
+Projeto didático utilizado para meu curso de Django  - Construa um Ecommerce com Python 3 e Django
+
+
+#Dados do Projeto
+
+* Django 1.9.6
+* Python 3.5.2
+* Site para testes em http://vini-ecommerce.herokuapp.com/
--- core/views.py
+++ core/views.py
@@ -4,23 +4,40 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.conf import settings
+from django.views.generic import View, TemplateView
 
 from catalog.models import Category
 from .forms import ContactForm
 
 
-def index(request):
-    context = {
-        'categories' : Category.objects.all()
-    }
-    return render(request, 'index.html', context)
+class IndexView(TemplateView):
+
+    template_name = 'index.html'
+
+
+# index = IndexView()
+index = IndexView.as_view()
+
+
+
+    #  se for fazer herança de IndexView(object):
+    # def __call__(self,request):
+    #     return render(request, 'index.html')
+
+
+
+    # def index(request):
+    #     context = {
+    #         'categories' : Category.objects.all()
+    #     }
+    #     return render(request, 'index.html', context)
 
 
 def contact(request):
     success = False
     form  = ContactForm(request.POST or None)
     if form.is_valid():
-        form.send_mail()            
+        form.send_mail()
         success = True
     context = {
         'form' : form,
