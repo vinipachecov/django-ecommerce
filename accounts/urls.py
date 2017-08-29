a11d16589d74dd3e29bf97568ee38a69d66d246a
vinicius pacheco <vinipachecov@gmail.com>
2017-08-29 09:40:30 -0300

    testes e modificações de password e conta do usuário

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,5 +10,8 @@ from . import views
 # arquivo views.py da aplicação accounts
 # poderia ser substituido por views.RegisterView.as_view
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^alterar_dados/$', views.update_user, name='update_user'),
+    url(r'^alterar_senha/$', views.update_password, name='update_password'),
     url(r'^registro/$', views.register, name='register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,23 @@
 
 
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import (
+    CreateView, TemplateView, UpdateView, FormView
+    )
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+# para a mudança do password
+from django.contrib.auth.forms import PasswordChangeForm
+
 
 from .models import User
 from .forms import UserAdminCreationForm
+
+
+class IndexView( LoginRequiredMixin,TemplateView):
+
+    template_name = 'accounts/index.html'
+
 
 class RegisterView(CreateView):
     model = User
@@ -15,5 +27,36 @@ class RegisterView(CreateView):
     success_url = reverse_lazy('index')
 
 
+class UpdateUserView(LoginRequiredMixin, UpdateView):
 
+    model = User
+    template_name = 'accounts/update_user.html'
+    fields = ['name','email']
+    success_url = reverse_lazy('accounts:index')
+
+    def get_object(self):
+        # procura um id ou slug na url e tenta resgatar atraves do model_mommy
+        return self.request.user
+
+
+class UpdatePasswordView(LoginRequiredMixin, FormView ):
+
+    template_name = 'accounts/update_password.html'
+    success_url = reverse_lazy('accounts:index')
+    form_class = PasswordChangeForm
+
+    #  gera os argumentos que vão ser passados para o formulario
+    def get_form_kwargs(self):
+        kwargs = super(UpdatePasswordView , self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+
+    def form_valid(self, form):
+        form.save()
+        return super(UpdatePasswordView, self).form_valid(form)
+
+index = IndexView.as_view()
 register = RegisterView.as_view()
+update_user = UpdateUserView.as_view()
+update_password = UpdatePasswordView.as_view()
