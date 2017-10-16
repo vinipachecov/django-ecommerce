76b181eba64ef4856e661f288db8435ba72c17f0
vinicius p vieira <vinipachecov@gmail.com>
2017-10-16 20:47:49 -0200

    catalog items language settings

--- catalog/models.py
+++ catalog/models.py
@@ -6,15 +6,15 @@ from django.core.urlresolvers import reverse
 
 class Category(models.Model):
 
-    name = models.CharField('Nome', max_length=100)
-    slug = models.SlugField('Identificador', max_length=100)
+    name = models.CharField('Name', max_length=100)
+    slug = models.SlugField('ID', max_length=100)
 
-    created = models.DateTimeField('Criado em', auto_now_add=True)
-    modified = models.DateTimeField('Modificado em', auto_now=True)
+    created = models.DateTimeField('Created in', auto_now_add=True)
+    modified = models.DateTimeField('Modified in', auto_now=True)
 
     class Meta:
-        verbose_name = 'Categoria'
-        verbose_name_plural = 'Categorias'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
         ordering = ['name']
 
     def __str__(self):
@@ -26,19 +26,19 @@ class Category(models.Model):
 
 class Product(models.Model):
 
-    name = models.CharField('Nome', max_length=100)
-    slug = models.SlugField('Identificador', max_length=100)
-    category = models.ForeignKey('catalog.Category', verbose_name='Categoria')
-    description = models.TextField('Descrição', blank=True)
-    price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+    name = models.CharField('Name', max_length=100)
+    slug = models.SlugField('ID', max_length=100)
+    category = models.ForeignKey('catalog.Category', verbose_name='Category')
+    description = models.TextField('Description', blank=True)
+    price = models.DecimalField('Price', decimal_places=2, max_digits=8)
     image = models.ImageField(upload_to='products', blank=True, null=True)
 
-    created = models.DateTimeField('Criado em', auto_now_add=True)
-    modified = models.DateTimeField('Modificado em', auto_now=True)
+    created = models.DateTimeField('Created in ', auto_now_add=True)
+    modified = models.DateTimeField('Modified in', auto_now=True)
 
     class Meta:
-        verbose_name = 'Produto'
-        verbose_name_plural = 'Produtos'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
         ordering = ['name']
 
     def __str__(self):
