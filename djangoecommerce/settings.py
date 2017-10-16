ec293bb2933f8e5e3fe7d850cc94cb0e09b84169
vinicius p vieira <vinipachecov@gmail.com>
2017-10-16 20:38:35 -0200

    language update

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -119,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'eng'
 
 TIME_ZONE = 'America/Recife'
 
