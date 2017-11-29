898debdac52157b00726e6c21e47591a77f2ebfc
vinicius p vieira <vinipachecov@gmail.com>
2017-11-29 10:38:02 -0200

    fix bucket name

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -188,10 +188,10 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
 AWS_STORAGE_BUCKET_NAME = 'viniecommerce'
 AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
 
-STATICFILES_STORAGE = 'djangoecommerce.s3util.StaticStorage'
+STATICFILES_STORAGE = 'viniecommerce.s3util.StaticStorage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'djangoecommerce.s3util.MediaStorage'
+DEFAULT_FILE_STORAGE = 'viniecommerce.s3util.MediaStorage'
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 AWS_HEADERS = {
